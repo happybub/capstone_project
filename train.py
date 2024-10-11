a4329de3366da01270b14c5a6506a3c32f12fd04
LoveTianyi <vocaloidluotianyi@qq.com>
2024-10-11 09:01:04 +0800

    add the kl_divergence, restore the train epoch and create the train epoch debug mode

--- train.py
+++ train.py
@@ -8,8 +8,81 @@ from utils import load_class_by_name, get_config, pop_up_image
 from dataset.dataloader import get_dataloader
 from modules.model import OurModel, OurModel_DEBUG
 
-
 def train_epoch(net, optim, dataloader_map, config, epoch, mode='train'):
+    print_time = config['PRINT_TIME']
+    net.print_time = print_time
+    if mode == 'train':
+        dataloader = dataloader_map['train']
+        net.train()
+    elif mode == 'val':
+        dataloader = dataloader_map['val']
+        net.eval()
+    else:
+        raise ValueError('mode should be either train or val')
+
+    device = config['DEVICE']
+    num_bits = int(config['NUM_BITS'])
+    lambda_image_loss = float(config['LAMBDA_IMAGE_LOSS'])
+    lambda_secret_loss = float(config['LAMBDA_SECRET_LOSS'])
+
+    image_losses = []
+    secret_losses = []
+
+    for i, images in enumerate(dataloader):
+        # get the host images
+        images = images.to(device=device)
+
+        # generate the secrets message
+        batch = images.size(0)
+        secret = torch.randint(0, 2, (batch, num_bits)).float().to(device=device)
+
+        with torch.set_grad_enabled(mode == 'train'):
+
+            # forward pass
+            container_image, freq_noise = net(secret, images)
+
+            # attack the images
+            attacked_image = net.attack_image(container_image)
+
+            # recover the secret message
+            recovered_secret, r_freq_noise = net.reverse(attacked_image)
+
+            # calculate the loss
+            if print_time:
+                print(f"calculating the loss: {time.time()}")
+            image_loss = mse_loss(container_image, images)
+            secret_loss = mse_loss(recovered_secret, secret)
+
+            if print_time:
+                print(f"loss calculated: {time.time()}")
+
+            total_loss = lambda_image_loss * image_loss + lambda_secret_loss * secret_loss
+
+            # backward pass
+            if mode == 'train':
+                if print_time:
+                    print(f"start backward: {time.time()}")
+                optim.zero_grad()
+                total_loss.backward()
+                optim.step()
+                if print_time:
+                    print(f"end backward: {time.time()}")
+
+        image_losses.append(image_loss.item())
+        secret_losses.append(secret_loss.item())
+
+        print(f'Batch: #{i}, Image Loss: {image_loss.item()}, Secret Loss: {secret_loss.item()}, Total Loss: {total_loss}')
+
+    log_dir = config['LOG_DIR']
+    # create the log file
+    os.makedirs(os.path.join(log_dir, f'mode {mode} epoch {epoch}'), exist_ok=True)
+    with open(os.path.join(log_dir, f'mode {mode} epoch {epoch}', 'image_loss_log.txt'), 'w') as f:
+        f.write('\n'.join([str(item) for item in image_losses]))
+    with open(os.path.join(log_dir, f'mode {mode} epoch {epoch}', 'secret_loss_log.txt'), 'w') as f:
+        f.write('\n'.join([str(item) for item in secret_losses]))
+
+
+def train_epoch_DEBUG(net, optim, dataloader_map, config, epoch, mode='train'):
     print_time = config['PRINT_TIME']
     net.print_time = print_time
     if mode == 'train':
@@ -60,10 +133,23 @@ def train_epoch(net, optim, dataloader_map, config, epoch, mode='train'):
             # ADD distribution noise
             distribution_loss = mse_loss(freq_noise, r_freq_noise)
 
+            ## calculate kl divergence
+            freq_noise_prob = freq_noise / freq_noise.sum()
+            r_freq_noise_prob = r_freq_noise / r_freq_noise.sum()
+
+            eps = 1e-10
+            freq_noise_prob = torch.clamp(freq_noise_prob, eps, 1)
+            r_freq_noise_prob = torch.clamp(r_freq_noise_prob, eps, 1)
+
+            kl_divergence = torch.nn.functional.kl_div(
+                freq_noise_prob.log(), r_freq_noise_prob, reduction='batchmean'
+            )
+
             if print_time:
                 print(f"loss calculated: {time.time()}")
 
-            total_loss = lambda_image_loss * image_loss + lambda_secret_loss * secret_loss + lambda_distribution_loss * distribution_loss
+            # total_loss = lambda_image_loss * image_loss + lambda_secret_loss * secret_loss + lambda_distribution_loss * distribution_loss
+            total_loss = lambda_image_loss * image_loss + lambda_secret_loss * secret_loss + lambda_distribution_loss * kl_divergence
 
             # backward pass
             if mode == 'train':
@@ -79,7 +165,8 @@ def train_epoch(net, optim, dataloader_map, config, epoch, mode='train'):
         secret_losses.append(secret_loss.item())
 
         distribution_losses.append(distribution_loss.item())
-        print(f'Batch: #{i}, Image Loss: {image_loss.item()}, Secret Loss: {secret_loss.item()}, Distribution Loss: {distribution_loss.item()}, Total Loss: {total_loss}')
+        # print(f'Batch: #{i}, Image Loss: {image_loss.item()}, Secret Loss: {secret_loss.item()}, Distribution Loss: {distribution_loss.item()}, Total Loss: {total_loss}')
+        print(f'Batch: #{i}, Image Loss: {image_loss.item()}, Secret Loss: {secret_loss.item()}, KL divergence: {kl_divergence.item()}, Total Loss: {total_loss}')
 
     log_dir = config['LOG_DIR']
     # create the log file
@@ -133,10 +220,10 @@ def train(name, start_epoch, end_epoch, config):
             print(f'Load the model and the optimizer from {epoch - 1}.pth')
 
         # continue the training
-        train_epoch(net, optim, dataloader_map, config, epoch, mode='train')
+        train_epoch_DEBUG(net, optim, dataloader_map, config, epoch, mode='train')
 
         # validate the model
-        train_epoch(net, optim, dataloader_map, config, epoch, mode='val')
+        train_epoch_DEBUG(net, optim, dataloader_map, config, epoch, mode='val')
 
         # save the state dict
         save_freq = config['SAVE_FREQ']
