aac9df21691ce2ea6c1fa7f5980afa265dc38e61
glingy <gglingy@gmail.com>
2024-11-11 10:14:58 +0800

    GAN

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ LAMBDA_SECRET_LOSS = 0.5
 
 SAVE_FREQ = 10
 
-NUM_BITS = 112 * 112 / 2
+NUM_BITS = 112 * 112
 DEVICE = 'mps'
 
 TEXT_EMBEDDING_MODULE = 'modules.text_embedding.SimpleTextEmbedding'
--- modules/model.py
+++ modules/model.py
@@ -337,7 +337,7 @@ class INV_block(nn.Module):
 
         self.clamp = clamp
         # ρ
-        # self.r = ResidualDenseBlock_out((self.channels_x + self.channels_y), (self.channels_x + self.channels_y))
+        # self.y = ResidualDenseBlock_out(self.channels_x, channels_y)
         # self.r = ResidualVitBlockQKV2(q_channels=channels_y, kv_channels=channels_x, query_first=False)
         self.r = ChannelSelfAttention(in_channels=self.channels_x, out_channels=self.channels_y)
         # η
@@ -346,7 +346,7 @@ class INV_block(nn.Module):
         # self.y = ResidualDenseBlock_out(q_channels=channels_y, kv_channels=channels_x, query_first=False)
         # self.y = ResidualDenseBlock_out(self.channels_x, self.channels_y)
         # φ
-        # self.f = ResidualDenseBlock_out((self.channels_x + self.channels_y), (self.channels_x + self.channels_y))
+        # self.f = self.y = ResidualDenseBlock_out(self.channels_y, channels_x)
         self.f = SAVit(in_channels=self.channels_y, out_channels=self.channels_x)
 
     def e(self, s):
